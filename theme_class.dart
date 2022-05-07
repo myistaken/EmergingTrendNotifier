39cede084c462cba63f7d98c1d795bad7ad84661
Yusuf Bilgin <93447964+CyberSoldierr@users.noreply.github.com>
2022-05-07 10:55:27 +0300

    Delete theme_class.dart

--- theme_class.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ThemeClass {
-  ThemeData lightTheme =
-  ThemeData(brightness: Brightness.light, primaryColor: Colors.blue);
-
-  ThemeData darkTheme =
-  ThemeData(brightness: Brightness.dark, primaryColor: Colors.black);
-}
