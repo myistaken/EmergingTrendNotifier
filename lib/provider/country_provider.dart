b06f308e8d8f9f18fd578b6d6b2f0fd9eca33c1a
Yusuf Bilgin <93447964+CyberSoldierr@users.noreply.github.com>
2022-05-28 16:42:01 +0300

    Update country_provider.dart

--- lib/provider/country_provider.dart
+++ lib/provider/country_provider.dart
@@ -1,20 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../dataBase/authantication.dart';
 import '/models/country.dart';
 import 'package:flutter/material.dart';
 
 class CountryProvider with ChangeNotifier {
-  List<Countries> initialData = [];
-
+  List<Countries> initialData =  [];
   final List<Countries> _counts = [];
-
   List<Countries> get counts => _counts;
-
+  List ls=[];
+  void initData(){
+    FirebaseFirestore.instance.collection("lists").doc(Authentication().userUID).get().then((value){
+      value.data()?.forEach((key, value) {value.forEach(
+              (data){
+                ls.add(data.toString());
+                List dataList=data.toString().split("*");
+                _counts.add(Countries(countryName: dataList[1], woeid: int.parse(dataList[2]), code: dataList[0]));
+          }
+      );});
+    });
+  }
   void addToList(Countries country) {
     _counts.add(country);
+
+    String s=country.code+""+country.countryName+""+country.woeid.toString();
+    ls.add(s);
+    FirebaseFirestore.instance.collection('lists').doc(Authentication().userUID).set({
+      'list':ls
+    });
     notifyListeners();
   }
 
   void removeFromList(int index) {
     _counts.removeAt(index);
+    ls.removeAt(index);
+    FirebaseFirestore.instance.collection('lists').doc(Authentication().userUID).set({
+      'list': ls
+    });
     notifyListeners();
   }
 }
